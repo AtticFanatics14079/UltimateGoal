9a467495239a05ae3a589b25bc6b424c1a36dfc4
JonCoombs <22coombs2@lexingtonma.org>
2020-11-29 16:08:55 -0500

    Mid changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JONSKETCH/DriveObjectV2/DriveObject.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JONSKETCH/DriveObjectV2/DriveObject.java
@@ -2,15 +2,8 @@ package org.firstinspires.ftc.teamcode.JONSKETCH.DriveObjectV2;
 
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
-public abstract class DriveObject {
+public interface DriveObject {
 
-    static ValueStorage vals;
-    int partNum;
-
-    public DriveObject(ValueStorage vals, HardwareMap hwMap, String objectName, int partNum) {};
-    void set(double value) {
-
-    };
     int getPartNum();
     double[] get();
     void setHardware(double value);
