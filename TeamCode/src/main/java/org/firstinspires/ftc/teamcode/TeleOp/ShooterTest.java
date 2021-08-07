59909e2be1f64dba2c6364300e9a787dd90d7a11
Austin <22li43@lexingtonma.org>
2021-08-07 12:27:56 -0400

    Auto stash before merge of "main" and "origin/main"

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/ShooterTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/ShooterTest.java
@@ -1,6 +1,8 @@
 package org.firstinspires.ftc.teamcode.TeleOp;
 
+import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
@@ -18,10 +20,14 @@ public class ShooterTest extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         shooterTestConfig config = new shooterTestConfig();
+        FtcDashboard dashboard = FtcDashboard.getInstance();
         config.Configure(hardwareMap);
         waitForStart();
+
         while(!isStopRequested()) {
             config.shooter.setPower(power);
         }
+
+
     }
 }
